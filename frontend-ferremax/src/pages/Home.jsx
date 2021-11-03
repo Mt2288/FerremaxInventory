919fe77dd1084f6ee5dfd5f4e0d25608666ce5d0
Yimy Cohetata Pedraza <jiimmypedrazz@gmail.com>
2021-11-02 21:46:52 -0500

    implementacion dashboard 2

---
+++ frontend-ferremax/src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800"><i className="fas fa-home"></i> Inicio</h1>
+      </div>
+      {/* <!-- Content Row --> */}
+      <div class="row">
+        {/* <!-- Earnings (Monthly) Card Example --> */}
+        <div class="col-xl-6 col-md-6 mb-4">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam tenetur expedita ad vitae harum assumenda enim error perferendis unde veritatis animi, culpa, dignissimos, odit accusantium fuga blanditiis rem cum impedit.</p>
+        </div>
+
+        {/* <!-- Earnings (Monthly) Card Example --> */}
+        <div class="col-xl-6 col-md-6 mb-4">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam reiciendis nihil eius quia sunt atque assumenda, incidunt odio quos, odit officiis voluptatibus cumque culpa fuga facilis placeat inventore est? Harum.</p>
+        </div>
+
+    
+      </div>
+
+  
+    </React.Fragment>
+  );
+};
+
+export default Home;
---
+++ frontend-ferremax/src/pages/Product.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Product = () => {
+  return (
+    <React.Fragment>
+      <h1 class="h3 mb-2 text-gray-800">Productos</h1>
+    </React.Fragment>
+  );
+};
+
+export default Product;
